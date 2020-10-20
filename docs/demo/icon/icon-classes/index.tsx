5bd2ee7dc11ce49642622056928bd32e1d9013ef
milo ma <milobluebell@gmail.com>
2020-10-20 14:42:25 +0800

    fix(doc): 修改文档build时样式被treeshaking掉的问题，pr模版增加一项检查内容

--- docs/demo/icon/icon-classes/index.tsx
+++ docs/demo/icon/icon-classes/index.tsx
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Icon, message, Input, Affix } from '@mlz/admin';
+import { Icon, message, Input, Affix, Spin } from '@mlz/admin';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
-import './icon-classes.less';
+import './index.less';
 
 const genContent = ($className: string) => `<Icon type="${$className}" />`;
 const { Search } = Input;
 const IconClasses = () => {
   const [origin, setOrigin] = useState<any>([]);
   const [items, setItems] = useState<any>([]);
+  const [loading, toggleLoading] = useState<boolean>(true);
   const [searchKey, setSearchKey] = useState<string>('');
   useEffect(() => {
     axios.get('https://service-81ozmkay-1252070958.gz.apigw.tencentcs.com/release/fetch_icon_classes').then((res) => {
       setItems(res.data);
       setOrigin(res.data);
+      toggleLoading(false);
     });
   }, []);
   const copiedHandler = ($className: string) => {
@@ -29,20 +31,22 @@ const IconClasses = () => {
           <Search placeholder="搜索icon class" onChange={(e) => setSearchKey(e.target.value)} enterButton="搜索" />
         </div>
       </Affix>
-      <div className="icon-classes-wrapper">
-        {items.map((item) => {
-          return (
-            <CopyToClipboard text={genContent(item)} onCopy={() => copiedHandler(item)}>
-              <div className="icon-class-container">
-                <Icon type={item} style={{ fontSize: 32 }} />
-                <span>{item.toString()}</span>
-              </div>
-            </CopyToClipboard>
-          );
-        })}
-        {items.length <= 0 ? '没有找到数据' : null}
-        <div className="mask" />
-      </div>
+      <Spin spinning={loading}>
+        <div className="icon-classes-wrapper">
+          {items.map((item) => {
+            return (
+              <CopyToClipboard text={genContent(item)} onCopy={() => copiedHandler(item)}>
+                <div className="icon-class-container">
+                  <Icon type={item} style={{ fontSize: 32 }} />
+                  <span>{item.toString()}</span>
+                </div>
+              </CopyToClipboard>
+            );
+          })}
+          {items.length <= 0 ? '没有找到数据' : null}
+          <div className="mask" />
+        </div>
+      </Spin>
     </>
   );
 };
