91e15a32a8270e57d11507e8693eeb9e71b5845d
milo ma <milobluebell@gmail.com>
2020-10-20 13:43:04 +0800

    feat(#24): 完成 issue#24

---
+++ docs/demo/icon/icon-classes.tsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Icon, message, Input, Affix } from '@mlz/admin';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import './icon-classes.less';
+
+const genContent = ($className: string) => `<Icon type="${$className}" />`;
+const { Search } = Input;
+const IconClasses = () => {
+  const [origin, setOrigin] = useState<any>([]);
+  const [items, setItems] = useState<any>([]);
+  const [searchKey, setSearchKey] = useState<string>('');
+  useEffect(() => {
+    axios.get('https://service-81ozmkay-1252070958.gz.apigw.tencentcs.com/release/fetch_icon_classes').then((res) => {
+      setItems(res.data);
+      setOrigin(res.data);
+    });
+  }, []);
+  const copiedHandler = ($className: string) => {
+    message.success($className.substr(0, $className.lastIndexOf('_')) + ' 复制成功');
+  };
+  useEffect(() => {
+    setItems(origin.filter((item) => item.includes(searchKey)));
+  }, [searchKey]);
+  return (
+    <>
+      <Affix offsetTop={64}>
+        <div className="search-area">
+          <Search placeholder="搜索icon class" onChange={(e) => setSearchKey(e.target.value)} enterButton="搜索" />
+        </div>
+      </Affix>
+      <div className="icon-classes-wrapper">
+        {items.map((item) => {
+          return (
+            <CopyToClipboard text={genContent(item)} onCopy={() => copiedHandler(item)}>
+              <div className="icon-class-container">
+                <Icon type={item} style={{ fontSize: 32 }} />
+                <span>{item.toString()}</span>
+              </div>
+            </CopyToClipboard>
+          );
+        })}
+        {items.length <= 0 ? '没有找到数据' : null}
+        <div className="mask" />
+      </div>
+    </>
+  );
+};
+
+export default IconClasses;
--- docs/index.md
+++ docs/index.md
@@ -8,7 +8,7 @@ title: 介绍
 
 <div align="left">
 
-![Codecov](https://codecov.io/gh/milobluebell/mlz-admin/branch/master/graph/badge.svg) ![Codacy](https://app.codacy.com/project/badge/Grade/999d89d9099e41ef81b9af94c98a8726) ![Action Checkers](https://github.com/juicecube/mlz-admin/workflows/checker/badge.svg) ![Npm Version](https://img.shields.io/npm/v/@mlz/admin?color=42b983&label=%40mlz%2Fadmin&logo=42b983&logoColor=42b983)
+[![codecov](https://codecov.io/gh/juicecube/mlz-admin/branch/master/graph/badge.svg?token=ZNPL3AMQ7Z)](undefined) ![Codacy](https://app.codacy.com/project/badge/Grade/999d89d9099e41ef81b9af94c98a8726) ![Action Checkers](https://github.com/juicecube/mlz-admin/workflows/checker/badge.svg) ![Npm Version](https://img.shields.io/npm/v/@mlz/admin?color=42b983&label=%40mlz%2Fadmin&logo=42b983&logoColor=42b983)
 
 </div>
 
@@ -66,8 +66,12 @@ npm install @mlz/pack -D
 
 # 贡献指南
 
-https://github.com/juicecube/mlz-admin/blob/master/CONTRIBUTING.md
+欢迎贡献代码，但请先参阅：https://github.com/juicecube/mlz-admin/blob/master/CONTRIBUTING.md
+
+或加入我们的钉钉群，与我们分享你的想法和建议：
+
+<img src="https://raw.githubusercontent.com/milobluebell/imgs-repo/master/WechatIMG9.jpeg" width="346" alt="Dingtalk Qrcode"/>
 
 ---
 
-##### ❤️ 如果你喜欢本项目，希望为我们点一颗星：[Github](https://github.com/juicecube/mlz-admin)
+##### ❤️ 如果你也喜欢本项目，请为我们点一颗星：[Github](https://github.com/juicecube/mlz-admin)
