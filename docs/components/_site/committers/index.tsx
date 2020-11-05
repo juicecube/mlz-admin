b0637690d1714985bfc00d92d35a74522513f9b5
milo ma <milobluebell@gmail.com>
2020-11-05 16:32:48 +0800

    fix: 修改一些range-select样式

--- docs/components/_site/committers/index.tsx
+++ docs/components/_site/committers/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, CSSProperties } from 'react';
 import axios from 'axios';
 import { Avatar, Tooltip, Typography, Spin, Button, Icon, Affix } from '@mlz/admin';
 import { uniqBy } from 'lodash-es';
@@ -7,6 +7,23 @@ import './index.less';
 
 const getDocCommits = ($path: string) => axios.get('https://api.github.com/repos/juicecube/mlz-admin/commits?per_page=100&path=' + $path + '&sha=master');
 const getCompCommits = ($path: string) => axios.get('https://api.github.com/repos/juicecube/mlz-admin/commits?per_page=100&path=' + $path + '&sha=master');
+const getAuthorFromData = ($oneCommit) => {
+  const author = $oneCommit.author || $oneCommit.commit.author;
+  return {
+    id: author.id || author.name,
+    name: author.login || author.name,
+    html_url: author.html_url,
+    avatar_url: author.avatar_url || author.name,
+  };
+};
+const TheAvatar = (props: { item: any; alternatives: string[]; rand: number; style?: CSSProperties }) => {
+  const { item, alternatives, rand, style } = props;
+  return (
+    <Avatar src={item.avatar_url} style={{ marginRight: 2, backgroundColor: alternatives[rand], ...style }}>
+      {item.avatar_url[0].toUpperCase()}
+    </Avatar>
+  );
+};
 export default () => {
   const [commits, setCommits] = useState<any[]>([]);
   const [loading, toggoleLoading] = useState(true);
@@ -14,10 +31,10 @@ export default () => {
   useEffect(() => {
     Promise.all([getDocCommits(docPath), getCompCommits(componentPath)])
       .then((res: any[]) => {
-        setCommits([res[0].data, res[1].data]);
+        setCommits(res.map((res_i) => res_i.data.map((commit) => getAuthorFromData(commit))));
         toggoleLoading(false);
       })
-      .catch((err) => toggoleLoading(false));
+      .catch(() => toggoleLoading(false));
   }, []);
   return (
     <div className="committers-wrapper">
@@ -28,34 +45,41 @@ export default () => {
       </Affix>
       <Spin spinning={loading}>
         <ul>
-          {commits.map((commitType, index) => {
-            const commiters = uniqBy(
-              commitType.map((_) => _.author),
-              'id',
-            );
-            return (
-              <li key={index}>
-                <span className="commit-title">
-                  <Typography.Text type="secondary">{index === 0 ? '文档贡献者' : '组件贡献者'}：</Typography.Text>
-                </span>
-                <span className="committers">
-                  <Avatar.Group>
-                    {(commiters.length &&
-                      commiters.map((item: any) => {
-                        return item ? (
-                          <Tooltip title={item.login} placement={index === 0 ? 'top' : 'bottom'} key={item.login}>
-                            <a href={item.html_url} target="_blank">
-                              <Avatar src={item.avatar_url} style={{ marginRight: 3 }} />
-                            </a>
-                          </Tooltip>
-                        ) : null;
-                      })) ||
-                      '--'}
-                  </Avatar.Group>
-                </span>
-              </li>
-            );
-          })}
+          {(() => {
+            const alternatives = ['#1890ff', '#00a2ae', '#ffbf00', '#a9d5fe', '#7265e6'];
+            const rand = parseInt(Math.random() * alternatives.length + '', 10);
+            return commits.map((commitType, index) => {
+              const commiters = uniqBy(commitType, 'id');
+              return (
+                <li key={index}>
+                  <span className="commit-title">
+                    <Typography.Text type="secondary">{index === 0 ? '文档贡献者' : '组件贡献者'}：</Typography.Text>
+                  </span>
+                  <span className="committers">
+                    <Avatar.Group>
+                      {(commiters.length &&
+                        commiters.map((item: any) => {
+                          return item ? (
+                            <Tooltip title={item.name} placement={index === 0 ? 'top' : 'bottom'} key={item.login}>
+                              {item.html_url ? (
+                                <a href={item.html_url} target="_blank">
+                                  <TheAvatar {...{ rand, alternatives, item }} />
+                                </a>
+                              ) : (
+                                <span>
+                                  <TheAvatar {...{ rand, alternatives, item }} style={{ opacity: 0.5 }} />
+                                </span>
+                              )}
+                            </Tooltip>
+                          ) : null;
+                        })) ||
+                        '--'}
+                    </Avatar.Group>
+                  </span>
+                </li>
+              );
+            });
+          })()}
         </ul>
       </Spin>
     </div>
--- docs/components/interactivity/select-range.md
+++ docs/components/interactivity/select-range.md
@@ -10,13 +10,17 @@
 - 当需要模糊筛选下拉项。
 - 数据需要先排好序传入
 
-### Api 说明
+### 基本使用
+
+<code src="./../../demo/select-range/normal-usage.demo.tsx" />
+
+### Apis
 
 | 参数          | 说明                                        | 类型                                             | 默认值     |
 | ------------- | ------------------------------------------- | ------------------------------------------------ | ---------- |
 | selectOptions | 下拉项数据                                  | [RangeOption](#RangeOption)\[]                   | []         |
 | currentValue  | 默认选中的值(相当于 Select 的 defaultValue) | [number\|string, number\|string]                 | []         |
-| width         | 一个展示框的宽度，前后选择框宽度一致        | number                                           | 160        |
+| width         | 一个展示框的宽度，前后选择框宽度一致        | number                                           | `160`      |
 | onChange      | 选中变化时触发                              | (value:[number\|string, number\|string]) => void | () => void |
 
 ### RangeOption
@@ -26,6 +30,12 @@
 | key  | Select 的唯一 value 值 | number\|string | -      |
 | name | Select 下拉项的展示值  | number\|string | -      |
 
-### 基本使用
+```tsx
+/**
+ * inline: true
+ */
+import React from 'react';
+import Commiters from '../_site/committers';
 
-<code src="./../../demo/select-range/normal-usage.demo.tsx" />
+export default () => <Commiters />;
+```
