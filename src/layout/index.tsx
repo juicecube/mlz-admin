338aa9a9a30a94f56bf47a8117169282053994be
milo ma <milobluebell@gmail.com>
2020-12-19 01:29:57 +0800

    feat(ErrorBoundary): 新增ErrorBoundary组件，在Layout.Content.errorBoundary不为false的情况下挂载在Content

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,2 +1,16 @@
 import React from 'react';
-export { Layout as default } from 'antd';
+import { Layout as AntdLayout } from 'antd';
+import ErrorBoundary from '../error-boundary';
+
+type AntdContentProsp = typeof Content;
+interface ContentProps extends AntdContentProsp {
+  errorBoundary?: boolean;
+}
+
+const { Content: AntdContent } = AntdLayout;
+const Content = (props: ContentProps): typeof Content => {
+  const { children, errorBoundary } = props;
+  return errorBoundary !== false ? <ErrorBoundary>{children}</ErrorBoundary> : children;
+};
+AntdLayout.Content = Content;
+export default AntdLayout;
