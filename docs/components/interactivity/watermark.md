7d5c9e0d552fe1972ab19c21cb4888b5c07b7893
milobluebell <milobluebell@gmail.com>
2020-12-31 01:48:42 +0800

    feat(Watermark): 完成watermark需求

--- docs/components/interactivity/watermark.md
+++ docs/components/interactivity/watermark.md
@@ -1,6 +1,6 @@
 # Watermark 水印
 
-** `📦 水印`用于为指定组件添加水印背景图片，保护数据权限。**
+** `📦 水印`用于为指定组件添加水印背景图片，声明数据隐私。**
 
 ## 基本使用
 
@@ -10,11 +10,12 @@
  * desc: 通过`text`字段为水印添加文字底板
  */
 import React from 'react';
-import { Watermark, Space } from '@mlz/admin';
+import { Watermark, Button } from '@mlz/admin';
 
 export default () => (
-  <Watermark text="版权所有©️milobluebell">
-    <div style={{ width: '100%', height: 240 }}>哦呵？</div>
+  <Watermark text="版权所有©️handsomest-man" loose={2} wrapGap={100}>
+    <div style={{ width: '100%', height: 400 }}>在他人不知情的情况下。即使不以盈利为目的，私自盗用他人肖像照片，依旧会被认定为违法行为。</div>
+    <Button type="danger">我知道了</Button>
   </Watermark>
 );
 ```
--- src/watermark/index.tsx
+++ src/watermark/index.tsx
@@ -1,28 +1,26 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { createBem } from '../shared/utils';
-import { WaterMarkProps } from './index.type';
+import { WaterMarkProps, DrawerParams } from './index.type';
+import { getDataType } from 'mytils';
 import './index.less';
 
-const bem = createBem('watermark');
-const frozenStyle = {
-  display: 'block',
-  wordWrap: 'normal',
-  transform: 'rotate(20deg)',
-} as const;
-const defaultStyle = {
-  textAlign: 'center',
-  fontWeight: 'normal',
-  fontColor: '#727071',
-} as const;
+const defaults = {
+  startX: 1,
+  startY: 0,
+  rotate: 30,
+  wrapGap: 80,
+  loose: 2.5,
+  fontSize: 12,
+  fontFamily: 'Georgia',
+};
 
-//
-const drawText = (text: string, canvasContext: CanvasRenderingContext2D, wrapperGap: number, dimension: { x: number; y: number }): CanvasRenderingContext2D => {
+const drawText = (text: string, canvasContext: CanvasRenderingContext2D, wrapperGap: number, dimension: DrawerParams): CanvasRenderingContext2D => {
+  const { fontSize = defaults.fontSize, fontFamily = defaults.fontFamily } = dimension.fontStyle || {};
   //参数说明
   //ctx：canvas的 2d 对象，t：绘制的文字，x,y:文字坐标，w：文字最大宽度
   let textCharArr = text.split('');
   let temp = '';
   let row: string[] = [];
-  const { x, y } = dimension;
+  const { startX = wrapperGap * defaults.startX, startY = defaults.startY, opacity, rotate = defaults.rotate, textAlign = 'center' } = dimension;
 
   for (let i = 0; i < textCharArr.length; i++) {
     if (canvasContext.measureText(temp).width < wrapperGap && canvasContext.measureText(temp + textCharArr[i]).width <= wrapperGap) {
@@ -33,39 +31,48 @@ const drawText = (text: string, canvasContext: CanvasRenderingContext2D, wrapper
     }
   }
   row.push(temp);
-  canvasContext.font = '12px Georgia';
-  canvasContext.globalAlpha = 0.2;
-  canvasContext.textAlign = 'center';
-  for (let b = 0; b < row.length; b++) {
-    console.log(row[b], '啊啊啊');
-    canvasContext.fillText(row[b], x, y + (b + 1) * 25); //每行字体y坐标间隔20
+  canvasContext.font = `${fontSize}px ${fontFamily}`;
+  canvasContext.globalAlpha = opacity || 0.2;
+  canvasContext.textAlign = textAlign;
+
+  // canvasContext.translate(wrapperGap * 2.5, y);
+  canvasContext.rotate((rotate * Math.PI) / 180);
+  // canvasContext.translate(-wrapperGap * 2.5, -y);
+
+  for (let i = 0; i < row.length; i++) {
+    canvasContext.fillText(row[i], startX, startY + (i + 1) * 22); //每行字体y坐标间隔20
   }
   return canvasContext;
 };
 
-const styleDecoratings = { position: 'relative', zIndex: 500 };
 const Watermark = (props: WaterMarkProps) => {
-  const { children, text } = props;
+  const { children, text, startX, startY = 0, wrapGap = defaults.wrapGap, opacity = 0.15, loose = defaults.loose, rotate = defaults.rotate, textAlign = 'center', fontStyle = {} } = props;
 
   const watermarkCanvas = useRef<HTMLCanvasElement>(null);
   const [update, forceUpdate] = useState(false);
 
   useEffect(() => {
-    const canvasContext = watermarkCanvas.current?.getContext('2d')!;
-    canvasContext && drawText(text, canvasContext, 50, { x: 50, y: 0 });
+    const canvasContext = watermarkCanvas.current?.getContext('2d');
+    canvasContext && drawText(text, canvasContext, wrapGap, { startX: startX || wrapGap, startY, opacity, fontStyle, rotate, textAlign });
     forceUpdate(!update);
   }, []);
+
+  const bgData = watermarkCanvas.current?.toDataURL();
+  const childrenBgData = children.props?.style?.background || '';
+  const limitedLoose = loose < 2 ? 2 : loose > 6 ? 6 : loose;
+
+  //
+  if (getDataType(children) === 'array') {
+    throw new Error('Watermark component should have only one children, but got ' + (children as any)?.length);
+  }
+
   return (
     <>
-      {React.cloneElement(
-        children,
-        {
-          ...children.props,
-          style: { ...styleDecoratings, ...children.props.style, background: `url(${watermarkCanvas.current?.toDataURL()})`, zIndex: styleDecoratings.zIndex - 1 },
-        },
-        <div className={bem('bg')}>{children}</div>,
-      )}
-      <canvas ref={watermarkCanvas} style={{ position: 'absolute', zIndex: -999, display: 'none' }} width={100}></canvas>
+      {React.cloneElement(children, {
+        ...children?.props,
+        style: { ...children?.props.style, background: bgData ? `url(${bgData})${childrenBgData ? ', ' + childrenBgData : ''}` : childrenBgData },
+      })}
+      <canvas ref={watermarkCanvas} style={{ position: 'absolute', zIndex: -999, display: 'none' }} width={wrapGap * limitedLoose}></canvas>
     </>
   );
 };
