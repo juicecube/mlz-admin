5c06a7d28e0e478895fc81dfeb71524c21d5b2c6
milo ma <milobluebell@gmail.com>
2021-01-22 01:41:45 +0800

    fix: 修改codecov配置和typings

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -2,7 +2,8 @@ type SupportingEnv = 'development' | 'production' | 'test' | 'staging' | 'previe
 declare interface Window extends global.Window {
   IconScripts: URL['href'][];
   __DEV__: SupportingEnv | undefined;
-  __MLZ_ADMIN_ENV__: SupportingEnv | undefined;
+  __MLZ_ADMIN_BUILD_ENV__: SupportingEnv | undefined;
+  __MLZ_ADMIN_RUNTIME_ENV__: 'development' | 'production' | 'test' | 'staging';
   __MLZ_ADMIN_VERSION__: string;
   requestIdleCallback?: (callback: () => void, options: any) => void;
 }
